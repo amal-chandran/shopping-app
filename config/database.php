85589bb40682c8a0f420b5a3636d8b073f4452fa
ADev <amalchandrandev@gmail.com>
2018-11-25 14:39:49 +0530

    added pgsql url parser

--- config/database.php
+++ config/database.php
@@ -1,5 +1,19 @@
 <?php
 
+$host = '127.0.0.1';
+$username = 'forge';
+$password = 'forge';
+$database = "";
+
+if (!empty(getenv("DATABASE_URL"))) {
+    $url = parse_url(getenv("DATABASE_URL"));
+
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+}
+
 return [
 
     /*
@@ -56,11 +70,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', $host),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', $database),
+            'username' => env('DB_USERNAME',$username),
+            'password' => env('DB_PASSWORD', $password),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
